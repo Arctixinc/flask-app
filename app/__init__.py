a21e96cbf63e162fe8c5e523f961c4156c9b5dee
Arctixinc <105559773+Arctixinc@users.noreply.github.com>
2023-04-22 19:26:42 +0530

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,7 @@
 from flask import Flask
 from .routes import api
-import os
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(api)
     return app
-print(f"Listening on port {os.environ['PORT']}")
