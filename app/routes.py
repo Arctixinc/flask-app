a8f7c49386d1adb94dbdad8f111a32887cbb305c
Sibu07 <91250474+Sibu07@users.noreply.github.com>
2023-04-22 17:57:05 +0530

    Add files via upload

--- app/routes.py
+++ app/routes.py
@@ -1,1 +1,33 @@
-.
+import json
+import requests
+from flask import Blueprint, request, Response
+
+api = Blueprint('api', __name__)
+
+@api.route('/data', methods=['GET'])
+def get_data():
+    json_url = request.args.get('json_url')
+    start = request.args.get('start')
+    end = request.args.get('end')
+    limit = request.args.get('limit')
+
+    response = requests.get(json_url)
+    json_data = json.loads(response.text)
+
+    if start:
+        start = int(start)
+    else:
+        start = 0
+
+    if end:
+        end = int(end)
+    else:
+        end = len(json_data)
+
+    if limit:
+        limit = int(limit)
+        end = start + limit
+
+    response_data = json_data[start:end]
+    response = Response(json.dumps(response_data), mimetype='application/json')
+    return response
